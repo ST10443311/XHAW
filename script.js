152edc8630ac66b038c76504d94f35f59b7a030e
ST10443311 <159542464+ST10443311@users.noreply.github.com>
2024-09-26 10:25:17 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function calculateTotal() {
-    let selectedCourses = document.getElementById('courses').selectedOptions;
-    let total = 0;
-    for (let option of selectedCourses) {
-        total += parseInt(option.value);
-    }
-    document.getElementById('total').innerText = total + ' R';
-}
